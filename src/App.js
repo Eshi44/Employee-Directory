27e46565a0be60b62f545bef748b30c071502d56
Eshi44 <e.hameliervolino@gmail.com>
2020-04-30 15:48:53 -0400

    added and header.js with a title and tested

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 // eslint-disable-next-line
 import React from "react";
 import Main from "./components/pages/main.jsx";
+import Header from "./components/pages/header.jsx";
 
 function App() {
-  return <Main />;
+  return (
+  <div>
+  <Header />
+  <Main />
+  </div>
+  )
 }
 
 export default App;
---
+++ src/components/pages/header.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <h4>Find an employee!</h4>
+        </div>
+    )
+}
+
+export default Header;
