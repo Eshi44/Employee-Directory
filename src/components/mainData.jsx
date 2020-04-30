c2c74acbf1c86ea2dd2266b744f46f957c889773
Eshi44 <e.hameliervolino@gmail.com>
2020-04-30 17:29:53 -0400

    added jsx files

---
+++ src/components/mainData.jsx
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from "react";
+import createContext from "src/components/createContext.jsx";
+
+const MainData = () => {
+    // eslint-disable-next-line no-unused-vars
+    const context = useContext(createContext);
+}
+export default MainData;
